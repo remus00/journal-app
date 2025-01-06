11db0622d584a484a565dffc13896c20a1f21b0a
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-01-06 20:55:22 +0100

    add all shadcn components needed

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
     return (
         <div className="flex h-screen w-full items-center justify-center">
-            <Button>Hello</Button>
+            <Button variant="journal">Hello</Button>
         </div>
     );
 }
