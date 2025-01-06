366455674be5a9b6cb1af41da67f8bbc87571797
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-01-06 21:04:49 +0100

    add a footer section

---
+++ components/custom/footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="bg-orange-300 bg-opacity-10 py-12">
+            <p className="mx-auto px-4 text-center text-gray-900">
+                Copyright &copy; Remus Burlacu &bull; All rights reserved
+            </p>
+        </footer>
+    );
+};
